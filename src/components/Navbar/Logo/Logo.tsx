3ea6d896f41be2afd2a3681dcc54daa28a82c0fa
Amandee Ellawala <amandee.manushika.e@gmail.com>
2026-05-06 13:30:28 +0100

    style: update app logo icon and apply theme-aware color styling

--- src/components/Navbar/Logo/Logo.tsx
+++ src/components/Navbar/Logo/Logo.tsx
@@ -1,10 +1,12 @@
-import LogoIcon from "../../../assets/logo/globe-clock-icon.svg?react";
+import LogoIcon from "../../../assets/logo/globe-clock-svgrepo-com.svg?react";
 
 function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <LogoIcon className="w-3.5 h-3.5 md:w-5 md:h-5 [&_path]:fill-white" />
-      <h1 className="md:text-sm text-xs font-sans-serif text-slate-800 dark:text-white">Timezone Clock</h1>
+      <LogoIcon className="w-3.5 h-3.5 md:w-5 md:h-5 [&_path]:fill-black dark:[&_path]:fill-white" />
+      <h1 className="md:text-sm text-xs font-sans-serif text-slate-800 dark:text-white">
+        Timezone Clock
+      </h1>
     </div>
   );
 }
